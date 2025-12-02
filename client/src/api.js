a5cbf9b95af6f3e0dd3c69dcfb9c0d03017c4fd3
Beastmeds <beastmeds@gmail.com>
2025-12-02 14:24:01 +0100

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+// Use Vite env `VITE_API_BASE` if set (recommended), otherwise fall back to server LAN IP.
+const API_BASE = (typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.VITE_API_BASE)
+  ? import.meta.env.VITE_API_BASE
+  : 'http://192.168.178.53:4000/api'
+
 export function createApi(token){
-  const instance = axios.create({ baseURL: 'http://localhost:4000/api' })
+  const instance = axios.create({ baseURL: API_BASE })
   if(token) instance.defaults.headers.common['Authorization'] = `Bearer ${token}`
   return instance
 }
